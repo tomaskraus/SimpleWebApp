66819bf08283dbe0f4c78d53f41a0e893fb79f93
tomaskraus <tomas.krausik@gmail.com>
2017-07-07 16:22:09 +0200

    add  some duration to the dummy backend

--- backend/btnPush.php
+++ backend/btnPush.php
@@ -6,6 +6,8 @@ $data = json_decode($post, true);
 
 $btnNumberStr = $data["buttonPressed"];
 
+sleep(1); //wait for 1 second, just to simulate some work
+
 header('Content-Type: application/json');
 echo '{ "status": "ok", "pressed": "' . $btnNumberStr . '"}';
 
--- backend/optionSelect.php
+++ backend/optionSelect.php
@@ -6,5 +6,7 @@ $data = json_decode($post, true);
 
 $optionSelectedStr = $data["optionSelected"];
 
+sleep(1); //wait for 1 second, just to simulate some work
+
 header('Content-Type: application/json');
 echo '{ "status": "ok", "optionSelected": "' . $optionSelectedStr . '"}';
